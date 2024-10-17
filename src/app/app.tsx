02f8924bf81007b91dedf9f957a1dd8a7f28fe80
siva-natarajan <siva-natarajan>
2024-10-17 22:12:17 +0100

    bootstrap table added

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -3,6 +3,7 @@ import './app.css';
 import Footer from './footer';
 import Header from './header';
 import HomePage from './homePage';
+import Table from 'react-bootstrap/Table';
 
 const App = () => {
   return (
@@ -11,6 +12,35 @@ const App = () => {
       <div className="content">
         <HomePage />
       </div>
+      <Table striped bordered hover size="sm">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan={2}>Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
       <Footer />
     </div>
   );
--- webpack.config.js
+++ webpack.config.js
@@ -3,51 +3,51 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 const htmlPlugin = new HtmlWebPackPlugin({
     template: "./index.html",
-  });
+});
 
-module.exports = 
-    {
-        entry: "./src/index.tsx",
-        mode: "development",
-        target: "web",
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: "client_bundle.js"
-        },
-        resolve: {
-            extensions: [".js", ".ts", ".tsx", ".jsx"]
-        },
-        module: {
-            rules: [
-                {
-                    test:/\.(ts|tsx)$/,
-                    exclude: /node_modules/,
-                    use: 'ts-loader',
+module.exports =
+{
+    entry: "./src/index.tsx",
+    mode: "development",
+    target: "web",
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "client_bundle.js"
+    },
+    resolve: {
+        extensions: [".js", ".ts", ".tsx", ".jsx"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: 'ts-loader',
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-typescript', '@babel/preset-react']
+                    }
                 },
-                {
-                    test:/\.(js|jsx)$/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['@babel/preset-env', '@babel/preset-typescript', '@babel/preset-react']
-                        }
-                    },
-                },
-                {
-                    test: /\.(png|jpeg)$/i,
-                    exclude: /node_modules/,
-                    type: "asset/resource",
-                },
-                {
-                    test: /\.(scss|css)$/,
-                    exclude: /node_modules/,
-                    use: ['style-loader', 'css-loader']
-                }
-            ]
-        },
-        plugins: [htmlPlugin],
-        devServer: {
-            hot: true
-        },
-    }
+            },
+            {
+                test: /\.(png|jpeg)$/i,
+                exclude: /node_modules/,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                // exclude: /node_modules/,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            }
+        ]
+    },
+    plugins: [htmlPlugin],
+    devServer: {
+        hot: true
+    },
+}
